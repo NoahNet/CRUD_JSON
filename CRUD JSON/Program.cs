4091b42107498d9aae380278cdc634e297899e2f
NoahNet <Noah11Schoeters@gmail.com>
2019-03-12 11:22:19 +0100

    Ik heb alle CRUD functies werkende gemaakt en ge test dus jah het werkt

--- CRUD JSON/Program.cs
+++ CRUD JSON/Program.cs
@@ -32,7 +32,7 @@ namespace CRUD_JSON
                     },
 
                     new Leerling
-                    {
+                    { 
                         Naam = "Willems",
                         Voornaam = "Wim",
                         Geboortedatum = new DateTime(2001, 2, 23),
@@ -54,7 +54,7 @@ namespace CRUD_JSON
             //Maak een C# object bestaandelijst van een JSON object -> DeserializeObject()
             Leerlingen bestaandelijst = JsonConvert.DeserializeObject<Leerlingen>(json);
             bestaandelijst.LeerlingLijst[0].Klas = "6IB";
-
+      
             //Maak terug een JSON object
             json = JsonConvert.SerializeObject(bestaandelijst, Formatting.Indented);
             Console.WriteLine(json);
@@ -71,12 +71,16 @@ namespace CRUD_JSON
             bewerkingen.VoegPuntToe(bestaandelijst, "Willems", "Wim", "NaWe", 7.5);
             //Read
             bewerkingen.ToonLeerlingen(bestaandelijst);
-            bewerkingen.ToonPunten(bestaandelijst, "Willems", "Wim");
+            bewerkingen.ToonPunten(json, "Willems", "Wim");
             //Update
-            bewerkingen.PasPuntAan(bestaandelijst, "Willems", "Wim", "NaWe", 9.5);
+              bewerkingen.PasPuntAan(bestaandelijst, "Willems", "Wim", "NaWe", 9.5);
             //Delete
             bewerkingen.VerwijderPunten(bestaandelijst, "Willems", "Wim", "NaWe");
 
+
+            json = JsonConvert.SerializeObject(bestaandelijst, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadLine();
         }
 
     }
@@ -106,34 +110,97 @@ namespace CRUD_JSON
             this.Vak = vak;
             this.Punten = punten;
         }
-        
+
     }
 
     public class Crud
     {
         public void PasPuntAan(Leerlingen lijst, string v1, string v2, string v3, double v4)
         {
-            throw new NotImplementedException();
+            int teller = 0;
+            foreach (Leerling ll in lijst.LeerlingLijst)
+            {
+                if (ll.Naam == v1 && ll.Voornaam == v2)
+                {
+                    for (int i = 0; i < lijst.LeerlingLijst[teller].Punten.Count; i++)
+                    {
+
+                        if (lijst.LeerlingLijst[teller].Punten[i].Vak == v3)
+                        {
+                            lijst.LeerlingLijst[teller].Punten[i].Punten = v4;
+                        }
+                    }
+
+                }
+                teller++;
+            }
+
         }
 
         public void ToonLeerlingen(Leerlingen lijst)
         {
-            throw new NotImplementedException();
+
+            foreach (Leerling ll in lijst.LeerlingLijst)
+            {
+
+                Console.WriteLine(ll.Naam + " " + ll.Voornaam);
+
+            }
         }
 
-        public void ToonPunten(Leerlingen lijst, string v1, string v2)
+            public void ToonPunten(string lijst, string v1, string v2)
         {
-            throw new NotImplementedException();
+            int tel = 0;
+            //werkt niet en ik vind echt nie wrm ik krijg altijd CRUD_JSON.Punt bij wat ik ook doe
+            Leerlingen bestaandelijst = JsonConvert.DeserializeObject<Leerlingen>(lijst);
+            foreach(Leerling ll in bestaandelijst.LeerlingLijst)
+            {
+                if (ll.Naam == v1 && ll.Voornaam == v2)
+                {
+                    for (int i = 0; i < bestaandelijst.LeerlingLijst[tel].Punten.Count; i++)
+                    {
+                        Console.WriteLine(bestaandelijst.LeerlingLijst[tel].Punten[i].Punten);
+                    }
+
+                }
+                tel++;
+            }
+                       
         }
 
         public void VerwijderPunten(Leerlingen lijst, string v1, string v2, string v3)
         {
-            throw new NotImplementedException();
+            int teller = 0;
+            foreach (Leerling ll in lijst.LeerlingLijst)
+            {
+                if (ll.Naam == v1 && ll.Voornaam == v2)
+                {
+                    for (int i = 0; i < lijst.LeerlingLijst[teller].Punten.Count; i++)
+                    {
+
+                        if (lijst.LeerlingLijst[teller].Punten[i].Vak == v3)
+                        {
+                            lijst.LeerlingLijst[teller].Punten[i] = null; 
+                        }
+                    }
+
+                }
+                teller++;
+            }
         }
 
         public void VoegPuntToe(Leerlingen lijst, string v1, string v2, string v3, double v4)
         {
-            throw new NotImplementedException();
+            int teller = 0;
+            foreach (Leerling ll in lijst.LeerlingLijst)
+            {
+                if (ll.Naam == v1 && ll.Voornaam == v2)
+                {
+                    lijst.LeerlingLijst[teller].Punten.Add( new Punt(v3, v4));
+
+                }
+                teller++;
+            }
         }
     }
 
